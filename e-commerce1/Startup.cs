65d3ef359ad9c51f1d472c8756eeceae92b820ec
aldo medina <aldomedina_10@hotmail.com>
2021-05-20 12:19:49 -0700

    Agregar archivos de proyecto.

---
+++ e-commerce1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_commerce1.Startup))]
+namespace e_commerce1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
