0503ffd0266de9afcbe104aac516ac867f5c3f86
aldo medina <aldomedina_10@hotmail.com>
2021-05-20 19:26:07 -0700

    Se agrego el controlador y vista de Ventas

---
+++ e-commerce1/Controllers/VentasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using e_commerce1.Models;
+
+namespace e_commerce1.Controllers
+{
+    public class VentasController : Controller
+    {
+        private e_tiendaEntities1 db = new e_tiendaEntities1();
+
+        // GET: Ventas
+        public ActionResult Index()
+        {
+            var ventas = db.Ventas.Include(v => v.Producto);
+            return View(ventas.ToList());
+        }
+
+        // GET: Ventas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ventas ventas = db.Ventas.Find(id);
+            if (ventas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ventas);
+        }
+
+        // GET: Ventas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Fk_Producto = new SelectList(db.Producto, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Ventas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Fk_Producto,Fk_Usuario,Fecha_Compra,Fecha_Entrega_Maxima,Cantidad,Total,Direccion_Entrega")] Ventas ventas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ventas.Add(ventas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Fk_Producto = new SelectList(db.Producto, "Id", "Nombre", ventas.Fk_Producto);
+            return View(ventas);
+        }
+
+        // GET: Ventas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ventas ventas = db.Ventas.Find(id);
+            if (ventas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Fk_Producto = new SelectList(db.Producto, "Id", "Nombre", ventas.Fk_Producto);
+            return View(ventas);
+        }
+
+        // POST: Ventas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Fk_Producto,Fk_Usuario,Fecha_Compra,Fecha_Entrega_Maxima,Cantidad,Total,Direccion_Entrega")] Ventas ventas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ventas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Fk_Producto = new SelectList(db.Producto, "Id", "Nombre", ventas.Fk_Producto);
+            return View(ventas);
+        }
+
+        // GET: Ventas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ventas ventas = db.Ventas.Find(id);
+            if (ventas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ventas);
+        }
+
+        // POST: Ventas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ventas ventas = db.Ventas.Find(id);
+            db.Ventas.Remove(ventas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
